bf1d89313363c1173634e38c13802495b328e90b
alfort74 <alfort625@gmail.com>
2017-07-08 20:51:20 +0900

    Changed generativeArt to draw perlin noise texture

--- src/generativeArt.cpp
+++ src/generativeArt.cpp
@@ -1,21 +1,24 @@
 #include "generativeArt.h"
 
-//--------------------------------------------------------------
-void generativeArt::setup(){
-    ofBackground(0, 0, 0);
-    ofSetFrameRate(60);
+
+void drawPerlinNoise(){
+    float value;
+    float scaleX = 0.007;		//1.0 / scaleX is coherence in x
+    float scaleY = 0.008;		//1.0 / scaleY is coherence in y
+    float posX0 = 593.2;
+    float posY0 = 43.7;
     
-    ofSetBackgroundAuto(false);
+    for (int y = 0; y < ofGetHeight(); y++){
+        for (int x = 0; x < ofGetWidth(); x++){
+//            value =ofNoise(x, y);
+            value = ofNoise(x*scaleX+posX0, y*scaleY+posY0);
+            ofSetColor(255*value, 255*value, 255*value);
+            ofDrawRectangle(x, y, 1, 1);
+        }
+    }
 }
 
-//--------------------------------------------------------------
-void generativeArt::update(){
-
-}
-
-//--------------------------------------------------------------
-void generativeArt::draw(){
-    
+void walkAndFade(RandomWalker walker[]){
     // Fade out
     ofEnableBlendMode(OF_BLENDMODE_ALPHA);
     ofSetColor(0, 0, 0, 3);
@@ -27,6 +30,27 @@ void generativeArt::draw(){
     for (int i = 0; i < 100; i++){
         walker[i].draw();
     }
+}
+
+//--------------------------------------------------------------
+void generativeArt::setup(){
+//    ofBackground(0, 0, 0);
+//    ofSetFrameRate(30);
+//    
+//    ofSetBackgroundAuto(false);
+    
+}
+
+//--------------------------------------------------------------
+void generativeArt::update(){
+
+}
+
+//--------------------------------------------------------------
+void generativeArt::draw(){
+//    walkAndFade(walker);
+    drawPerlinNoise();
+
 }
 
 //--------------------------------------------------------------
