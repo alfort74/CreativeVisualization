78ca081ace67f6479eea2a1171690becf86ff7c6
alfort74 <alfort625@gmail.com>
2017-07-09 16:48:01 +0900

    Changed to show map of airports

--- src/flightVisualization.cpp
+++ src/flightVisualization.cpp
@@ -3,21 +3,20 @@
 //--------------------------------------------------------------
 void flightVisualization::setup(){
     ofBackground(0);
+    ofSetBackgroundAuto(false);
+    ofSetColor(255, 255, 255);
+    ofSetCircleResolution(4);
+    ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, 50);
     // Display level of logs
     ofSetLogLevel("ofxCsv", OF_LOG_VERBOSE);
     // Load CSV file
     if(csv.load("airports.dat")) {
         // Output all data to log
         for(int i = 0; i < csv.getNumRows(); i++){
-//            for(int j = 0; j < csv.getNumCols(); j++){
-//                    cout << csv[i][j] << ", ";
-//            }
-//            cout << endl;
-            ofxCsvRow row = csv[i];
-            float latitude = row.getFloat(6);
-            float longitude = row.getFloat(7);
-            
-            cout << i << ": latitude = " << latitude << ", longitude = " << longitude << endl;
+            for(int j = 0; j < csv.getNumCols(); j++ ){
+                cout << i << ": " << csv[i][j];
+            }
+            cout << endl;
         }
     }
     
@@ -30,6 +29,30 @@ void flightVisualization::update(){
 
 //--------------------------------------------------------------
 void flightVisualization::draw(){
+    ofSetColor(255, 255, 255);
+    ofVec2f airportLoc;
+    
+    for(int i = 0; i < csv.getNumRows(); i++){
+        ofxCsvRow row = csv[i];
+        float latitude = row.getFloat(6);
+        float longitude = row.getFloat(7);
+        
+        airportLoc.x = ofMap(row.getFloat(7), -180, 180, 0, ofGetWidth());
+        airportLoc.y =ofMap(row.getFloat(6), 90, -90, 0, ofGetHeight());
+        
+        string type = row.getString(12);
+        if(type == "airport"){
+            ofSetColor(63, 127, 255);
+        } else if(type == "station"){
+            ofSetColor(63, 255, 127);
+        } else if(type == "port"){
+            ofSetColor(255, 127, 63);
+        } else {
+            ofSetColor(127);
+        }
+        
+        ofDrawCircle(airportLoc, 1.5);
+    }
 
 }
 
