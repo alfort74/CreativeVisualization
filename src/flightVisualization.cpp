91f276ae97a750fd3ec2043ecd3979c083562a34
alfort74 <alfort625@gmail.com>
2017-07-09 17:29:59 +0900

    Changed to use mesh for spherical view

--- src/flightVisualization.cpp
+++ src/flightVisualization.cpp
@@ -3,11 +3,22 @@
 //--------------------------------------------------------------
 void flightVisualization::setup(){
     ofBackground(0);
-    ofSetCircleResolution(4);
+    ofEnableBlendMode(OF_BLENDMODE_ADD);
+    ofSetCircleResolution(3);
+    
     // Display level of logs
     ofSetLogLevel("ofxCsv", OF_LOG_VERBOSE);
+    
+    // Display vertex of meshes as point
+    mesh.setMode(OF_PRIMITIVE_POINTS);
+    
+    // Change the size of points based on distance
+    static GLfloat distance[] = {0.0, 0.0, 1.0 };
+    glPointParameterfv(GL_POINT_DISTANCE_ATTENUATION, distance);
+    glPointSize(1500);
+    
     // Load CSV file
-    if(csv.load("airports.dat")) {
+    if(csv.load("airports-extended.dat")) {
         // Output all data to log
         for(int i = 0; i < csv.getNumRows(); i++){
             for(int j = 0; j < csv.getNumCols(); j++ ){
@@ -16,6 +27,39 @@ void flightVisualization::setup(){
             cout << endl;
         }
     }
+    
+    mesh.clear();
+    
+    for(int i = 0; i < csv.getNumRows(); i++){
+        ofxCsvRow row = csv[i];
+        float latitude = row.getFloat(6);
+        float longitude = row.getFloat(7);
+        
+        string type = row.getString(12);
+        ofFloatColor color;
+        if(type == "airport"){
+            color = ofFloatColor(0.2, 0.5, 1.0);
+        } else if(type == "station"){
+            color = ofFloatColor(1.0, 0.5, 0.2);
+        } else if(type == "port"){
+            color = ofFloatColor(0.5, 1.0, 0.2);
+        } else {
+            color = ofFloatColor(0.8);
+        }
+        
+        // Calculate the position on sphere based on latitude and longitude
+        ofQuaternion latRot, longRot, spinQuat;
+        latRot.makeRotate(row.getFloat(6), 1, 0, 0);
+        longRot.makeRotate(row.getFloat(7), 0, 1, 0);
+        spinQuat.makeRotate(ofGetFrameNum(), 0, 1, 0);
+        
+        ofVec3f radius = ofVec3f(0, 0, 300);
+        ofVec3f worldPoint = latRot * longRot * spinQuat * radius;
+        
+        mesh.addVertex(worldPoint);
+        mesh.addColor(color);
+    }
+
     
 }
 
@@ -26,42 +70,12 @@ void flightVisualization::update(){
 
 //--------------------------------------------------------------
 void flightVisualization::draw(){
-    ofSetColor(255, 255, 255);
     ofVec2f airportLoc;
     
     ofPushMatrix();
     ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
-    
-    for(int i = 0; i < csv.getNumRows(); i++){
-        ofxCsvRow row = csv[i];
-        float latitude = row.getFloat(6);
-        float longitude = row.getFloat(7);
-        
-        airportLoc.x = ofMap(row.getFloat(7), -180, 180, 0, ofGetWidth());
-        airportLoc.y =ofMap(row.getFloat(6), 90, -90, 0, ofGetHeight());
-        
-        string type = row.getString(12);
-        if(type == "airport"){
-            ofSetColor(63, 127, 255);
-        } else if(type == "station"){
-            ofSetColor(63, 255, 127);
-        } else if(type == "port"){
-            ofSetColor(255, 127, 63);
-        } else {
-            ofSetColor(127);
-        }
-        
-        // Calculate the position on sphere based on latitude and longitude
-        ofQuaternion latRot, longRot, spinQuat;
-        latRot.makeRotate(row.getFloat(6), 1, 0, 0);
-        longRot.makeRotate(row.getFloat(7), 0, 1, 0);
-        spinQuat.makeRotate(ofGetFrameNum(), 0, 1, 0);
-        
-        ofVec3f radius = ofVec3f(0, 0, ofGetHeight()/2.5);
-        ofVec3f worldPoint = latRot * longRot * spinQuat * radius;
-        
-        ofDrawCircle(worldPoint, 1.5);
-    }
+    ofRotateY(ofGetFrameNum());
+    mesh.draw();
     ofPopMatrix();
     
     ofSetColor(255);
